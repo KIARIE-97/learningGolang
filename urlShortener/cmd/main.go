2dc3057f9c01cc1cb4665801b28283059213b95f
sarah kiarie <sarahwanjiruki1@gmail.com>
2026-07-22 16:07:26 +0300

    phase two of project done

--- urlShortener/cmd/main.go
+++ urlShortener/cmd/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+/* struct- store the incoming url
+struct to store the shortened url
+*/
+
+type shortenRequest struct {
+	URL string `json:"url"`
+}
+type shortenResponse struct {
+	SHORTURL string `json:"short_url"`
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "URL shortener api is running")
 }
 
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	var req shortenRequest
+
+	err:= json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	response := shortenResponse{
+		SHORTURL: "http://localhost:8080/abc123",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main () {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/shorten", shortenHandler)
 
 	fmt.Println("server is running on port 8080")
 
